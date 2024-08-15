bd2a9d2b32a895381c660db0b294fdc82a9fca59
mithileshpatel888 <mithleshpatel887766@gmail.com>
2024-08-15 18:09:47 +0530

    create input component to write method to the currency chenge

--- src/app/components/InputBox.js
+++ src/app/components/InputBox.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React ,{useId} from 'react'
 
-const InputBox = () => {
-  return (
-    <div>
+function InputBox ({
+ lable,
+ Amount,
+ onAmountChange,
+ onCurrencyChange,
+ CurrencyOption = [],
+ selectCurrency = "USD",
+ AmountDisable,
+ CurrencyDisable,
+ className = "",
+}) 
+{
+    const amountInputId = useId()
+    return (
+         
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className='w-1/2'>
+            </div>
+            
+            <lable htmlFor={amountInputId} className= "text-black/40 mb-2 inline-block">
+            {lable}
+            </lable>
+
+            <input id={amountInputId} className='outline-none w-full bg-transparent py-1.5'
+            type='number'
+            placeholder='Amount'
+            disabled={AmountDisable}
+            value={Amount}
+            onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+            /> 
+
+            <div className='w-1/2 flex flex-wrap justify-end text-right'>
+            <p className='text-black/40 mb-2 w-full'>Currency Type</p>
+            <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+             value={selectCurrency}
+             onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+             disabled={CurrencyDisable}
+            >
+
+             {CurrencyOption.map((currency) =>  ( 
+            <option key={currency}  value={currency}>
+            {currency}
+             </option>
+            ))}
+
+            </select>
+            </div>
+            </div>
       
-    </div>
-  )
+    );
 }
-
 export default InputBox
